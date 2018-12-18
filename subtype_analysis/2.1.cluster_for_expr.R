3e63f243006bb7fef987ff79eff5d0814467bcf1
Huffyphenix <949985347@qq.com>
2018-12-18 19:15:07 +0800

    expr concensus cluster combined with survival data

--- subtype_analysis/2.1.cluster_for_expr.R
+++ subtype_analysis/2.1.cluster_for_expr.R
@@ -18,15 +18,20 @@ load(file = file.path(data_result_path, ".rda_IMK_mutationburden_cancerSubtype_a
 # index=which(is.na(PanCan26_gene_list_expr_matrix)) # no NA value
 
 ## Concencus clustering
-expr_cc <- ExecuteCC(clusterNum=3,d=PanCan26_gene_list_expr_matrix,maxK=10,clusterAlg="hc",distance="pearson",plot=F)
+expr_survival <- FSbyCox(PanCan26_gene_list_expr_matrix,expr_time,expr_status,cutoff=0.05)
+expr_cc <- ExecuteCC(clusterNum=3,d=expr_survival,maxK=10,clusterAlg="hc",distance="pearson",title = "expr_sur_cc")
+group_cc=expr_cc$group
+distanceMatrix_cc=expr_cc$distanceMatrix
+p_value=survAnalysis(mainTitle="expr_sur_result",expr_time,expr_status,group_cc,
+                     distanceMatrix_cc,similarity=TRUE)
 expr_cc %>% readr::write_rds(file.path(data_result_path, ".rds_PanCan28_expr_cc.rds.gz"), compress = 'gz')
 
 
 ## SNF
-expr_snf <- ExecuteSNF(PanCan26_gene_list_expr_matrix, clusterNum=3, K=20, alpha=0.5, t=20,plot = F)
-expr_snf %>% readr::write_rds(file.path(data_result_path, ".rds_PanCan28_expr_snf.rds.gz"), compress = 'gz')
+# expr_snf <- ExecuteSNF(PanCan26_gene_list_expr_matrix, clusterNum=3, K=20, alpha=0.5, t=20,plot = F)
+# expr_snf %>% readr::write_rds(file.path(data_result_path, ".rds_PanCan28_expr_snf.rds.gz"), compress = 'gz')
 
 ## SNF and CC
-expr_snfcc=ExecuteSNF.CC(PanCan26_gene_list_expr_matrix, clusterNum=3, K=20, alpha=0.5, t=20,maxK = 10, pItem = 0.8,reps=500, 
-                     plot = F, finalLinkage ="average") # what the meaning of result pic 11?12?13?
-expr_snfcc %>% readr::write_rds(file.path(data_result_path, ".rds_PanCan28_expr_snfcc.rds.gz"), compress = 'gz')
+# expr_snfcc=ExecuteSNF.CC(PanCan26_gene_list_expr_matrix, clusterNum=3, K=20, alpha=0.5, t=20,maxK = 10, pItem = 0.8,reps=500, 
+#                      plot = F, finalLinkage ="average") # what the meaning of result pic 11?12?13?
+# expr_snfcc %>% readr::write_rds(file.path(data_result_path, ".rds_PanCan28_expr_snfcc.rds.gz"), compress = 'gz')
