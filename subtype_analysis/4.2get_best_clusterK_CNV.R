80130add6005c49d59debd2673b4de7d2995ddbe
Huffyphenix <949985347@qq.com>
2019-01-01 10:16:54 +0800

    get best cluster K for each datasets

---
+++ subtype_analysis/4.2get_best_clusterK_CNV.R
@@ -0,0 +1,69 @@
+#load library
+library(methods)
+library(magrittr)
+library(CancerSubtypes)
+library(ConsensusClusterPlus)
+library(SNFtool)
+library(survival)
+library(survminer)
+library(ggplot2)
+
+# load data ---------------------------------------------------------------
+
+data_result_path <- "/project/huff/huff/immune_checkpoint/genelist_data"
+
+## get from 1.data_prepare.R
+load(file = file.path(data_result_path, ".rda_IMK_mutationburden_cancerSubtype_analysis.rda"))
+
+results <- readr::read_rds(file.path("/project/huff/huff/immune_checkpoint/genelist_data","genelist_CNV_CC_20.rds.gz"))
+
+# get best K for cluster results  -----------------------------------------
+source("/project/huff/huff/github/immune-cp/subtype_analysis/funtions_to_draw_pic.R")
+
+result_path <- file.path("/project/huff/huff/immune_checkpoint/result_20171025/subtype_analysis/CNV/Get_best_clutser_20")
+# pdf(file.path(result_path,"Get_best_clutser_20.pdf"))
+for (i in 2:20) {
+  C <- i
+  W <- results[[C]][['consensusMatrix']]
+  group <- results[[i]][['consensusClass']]
+  # Figure 1
+  # displayClusters(W, group)
+  
+  # Figure 2
+  group_statistic <- group %>% table()
+  less_than_10<- names(group_statistic[group_statistic<10])
+  all_clusters <- names(group_statistic)
+  more_than_10 <- setdiff(all_clusters,less_than_10)
+  data.frame(sample = names(group),group=group,time = cnv_time,status = cnv_status) %>%
+    dplyr::as.tbl() %>%
+    dplyr::filter(group %in% more_than_10) %>%
+    dplyr::rename("barcode"="sample") %>%
+    dplyr::left_join(cnv_merge_snv_data.cancer_info,by="barcode") -> group_survival_data
+  color_list = rainbow(length(more_than_10))
+  title <- paste("CNV Survival for",C,"Clusters",sep=" ")
+  sur_name <-  paste("CNV_Survival_for",C,"Clusters.png",sep="_")
+  if (length(more_than_10)>=2) {
+    fn_survival(group_survival_data,title,color_list,sur_name)
+  }else{
+    print("Too small groups")
+  }
+  
+  
+  # Figure 3
+  group_survival_data %>%
+    dplyr::left_join(mutation_burden_class,by="barcode") %>%
+    dplyr::mutate(sm_count = ifelse(is.na(sm_count),0,sm_count)) %>%
+    dplyr::rename("cancer_types"="cancer_types.x")-> group_cluster_mutation
+  comp_mat <- combn(more_than_10,2)
+  comp_list <- list()
+  m_name <-  paste("CNV_mutation_for",C,"Clusters.png",sep="_")
+  for (col in 1:ncol(comp_mat)) {
+    comp_list[[col]] <- comp_mat[,col]
+  }
+  fn_mutation_burden(group_cluster_mutation,color_list,comp_list,m_name)
+  
+  # Figure 4
+  m_a_name <-  paste("CNV_all_mutation_for",C,"Clusters.png",sep="_")
+  fn_mutation_burden_all(group_cluster_mutation,color_list,comp_list,m_a_name)
+}
+
