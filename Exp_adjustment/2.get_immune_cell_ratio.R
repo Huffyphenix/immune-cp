2734940712f921266cb5e9e993a648f947f051e3
Huffyphenix <949985347@qq.com>
2019-03-05 19:26:37 +0800

    re-run using result of xCell we run by 1.2xCell_run_step2.R, and correct the cell type names

--- Exp_adjustment/2.get_immune_cell_ratio.R
+++ Exp_adjustment/2.get_immune_cell_ratio.R
@@ -14,8 +14,16 @@ tcga_path <- file.path(immune_path,"/data/TCGA_data")
 
 # load data ---------------------------------------------------------------
 
-xCell_TCGA_RSEM <- readr::read_tsv(file.path(xcell_path,"xCell_TCGA_RSEM.txt")) %>%
-  tidyr::gather(-X1,key="barcode",value="score")
+xCell_TCGA_RSEM <- readr::read_rds(file.path(xcell_path,"Pan21_xCell_results.rds.gz")) 
+xCell_TCGA_RSEM %>%
+  dplyr::mutate(xCell_gather = purrr::map(xCell,.f=function(.x){
+    .x %>%
+      as.data.frame() %>%
+      dplyr::mutate(`Cell types` = rownames(.x)) %>%
+      tidyr::gather(-`Cell types`,key="barcode",value="score")
+  })) %>%
+  dplyr::select(-xCell) %>%
+  tidyr::unnest() -> xCell_TCGA_RSEM.gather
 
 xCell_immune_type <- readr::read_tsv(file.path(xcell_path,"cell_type_classification.13059_2017_1349_MOESM1_ESM.txt")) %>%
   dplyr::select(`Full name`,`Cell types`,Subgroup) %>%
@@ -26,8 +34,8 @@ xCell_cell_type <- readr::read_tsv(file.path(xcell_path,"cell_type_classificatio
 xCell_Fantom_cellname_adjust <- readr::read_tsv(file.path(xcell_path,"xCell_Fantom5_cellname_uniform.txt")) %>%
   dplyr::rename("Cell types" = "xCell_Cell types") %>%
   dplyr::inner_join(xCell_cell_type,by="Cell types") %>%
-  dplyr::mutate(Type = ifelse(Subgroup == "Lymphoid" | Subgroup == "Myeloid","Immune Cell","Stromal Cell")) %>%
-  dplyr::mutate(Type = ifelse(is.na(Fantom5_cellname),"Other stromal",Type)) %>%
+  dplyr::mutate(Type = ifelse(Subgroup == "Lymphoid" | Subgroup == "Myeloid","Immune Cell","Stromal Cell")) %>%  # fantom5 and xCell overlapped cells
+  dplyr::mutate(Type = ifelse(is.na(Fantom5_cellname),"Other stromal",Type)) %>% # cell in xCell but not in fantom5 are calss into other stromal.
   dplyr::select(`Cell types`,Type) %>%
   unique()
 
@@ -40,8 +48,7 @@ TCGA_tissue <- readr::read_tsv("/project/huff/huff/data/TCGA/TCGA_cancer_tissue_
 
 # data processing ---------------------------------------------------------
 
-xCell_TCGA_RSEM %>%
-  dplyr::rename("Cell types"= "X1") %>%
+xCell_TCGA_RSEM.gather %>%
   dplyr::inner_join(xCell_Fantom_cellname_adjust,by="Cell types") %>%
   dplyr::rename("Group"= "Type") %>%
   dplyr::group_by(barcode,Group) %>%
@@ -51,15 +58,19 @@ xCell_TCGA_RSEM %>%
   dplyr::ungroup() %>%
   tidyr::spread(key=Group,value=group_Score) -> xCell_TCGA_RSEM.immune_stroma.score
  
+
+# get ratio of each cell type ---------------------------------------------
+
 xCell_TCGA_RSEM.immune_stroma.score %>%
   dplyr::mutate(`Sample ID`=gsub(pattern = "\\.",replacement = "-",x = barcode)) %>%
   dplyr::inner_join(TCGA_purity,by="Sample ID") %>%
   dplyr::mutate(non_tumor_ratio = 1-CPE) %>%
-  dplyr::mutate(Immune_ratio=non_tumor_ratio*`Immune Cell`/(`Stromal Cell`+`Immune Cell`)) %>%
-  dplyr::mutate(Stromal_ratio=non_tumor_ratio*`Stromal Cell`/(`Stromal Cell`+`Immune Cell`)) -> xCell_TCGA_RSEM.immune_stroma.ratio
+  dplyr::mutate(Immune_ratio=non_tumor_ratio*`Immune Cell`/(`Stromal Cell`+`Immune Cell`+`Other stromal`)) %>%
+  dplyr::mutate(Stromal_ratio=non_tumor_ratio*`Stromal Cell`/(`Stromal Cell`+`Immune Cell`+`Other stromal`)) %>%
+  dplyr::mutate(other_Stromal_ratio=non_tumor_ratio*`Other stromal`/(`Stromal Cell`+`Immune Cell`+`Other stromal`)) -> xCell_TCGA_RSEM.immune_stroma.ratio
 
 xCell_TCGA_RSEM.immune_stroma.ratio %>%
-  readr::write_rds(file.path(immune_path,"genelist_data","xCell_TCGA_RSEM.immune_stroma.ratio.rds.gz"))
+  readr::write_rds(file.path(immune_path,"genelist_data","Pancan21.tcga.xCell.immune_stroma.ratio.rds.gz"))
 
 
 
