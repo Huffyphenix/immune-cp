6e6593dd9acc93317fa59461bdff9d739b360b8b
Huffyphenix <949985347@qq.com>
2019-03-18 09:22:31 +0800

    get best cluster number Kof scaled COCA result

---
+++ subtype_analysis/4.6.get_best_clusterK_COCA_scaled.R
@@ -0,0 +1,143 @@
+###########################################################
+# analysis of the result of combined cluster analysis for scaled exp, methylation clusters, and CNV raw data cluster results
+###########################################################
+
+library(SNFtool)
+library(ComplexHeatmap)
+library(magrittr)
+
+# get best K for cluster results  -----------------------------------------
+source("/project/huff/huff/github/immune-cp/subtype_analysis/funtions_to_draw_pic.R")
+
+result_path <- file.path("/project/huff/huff/immune_checkpoint/result_20171025/subtype_analysis")
+result_path <- file.path(result_path,"coca_scaled/Get_best_clutser_20")
+data_result_path <- "/project/huff/huff/immune_checkpoint/genelist_data"
+
+results <- readr::read_rds(path = file.path(data_result_path, ".nonscale_data.recode.combined.ConsensusClusterplus.rds.gz"))
+
+load(file = file.path(data_result_path, ".rda_genelist_data_survival_cancer.info.rda"))
+
+# get best K for cluster results  -----------------------------------------
+source("/project/huff/huff/github/immune-cp/subtype_analysis/funtions_to_draw_pic.R")
+
+mutation_burden_class <- readr::read_rds(file.path("/project/huff/huff/data/TCGA","classfication_of_26_cancers_by_mutation_burden192.rds.gz")) %>%
+  tidyr::unnest() %>%
+  dplyr::rename("cancer_types"="Cancer_Types")
+time_status %>%
+  dplyr::full_join(mutation_burden_class,by="barcode") %>%
+  dplyr::full_join(genelist_methy_mutaion_class.cancer_info,by="barcode") %>%
+  dplyr::mutate(cancer_types=ifelse(is.na(cancer_types.x),cancer_types.y,cancer_types.x)) %>%
+  dplyr::select(-cancer_types.x,-cancer_types.y) %>%
+  dplyr::rename("time"="PFS.time","status"="PFS")-> clinical_data
+
+color_20 <- c("#CDC0B0", "#838B8B", "#000000", "#0000FF", "#00008B", "#8A2BE2", "#A52A2A", "#FF4040", "#98F5FF", "#53868B", "#EEAD0E", "#458B00", "#EEA2AD", "#E066FF", "#EE3A8C", "#00FF00", "#FFFF00", "#5CACEE", "#8B6914", "#FF7F24")
+
+for(i in 2:20){
+  print(paste(i,1,sep = "."))
+  data_type <- "COCA"
+  C <- i
+  W <- results[[C]][['consensusMatrix']]
+  group <- results[[C]][['consensusClass']]
+  W <- matrix(W,nrow = length(group),dimnames = list(names(group),names(group)))
+  data.frame(sample = names(group),group=group) %>%
+    readr::write_tsv(file.path(result_path,paste(C,"clusters_group_info.tsv",sep="_")))
+  # Figure 1
+  # displayClusters(W, group)
+  print(paste(i,2,sep = "."))
+  
+  # Figure 2. survival
+  
+  group_statistic <- group %>% table()
+  less_than_10<- names(group_statistic[group_statistic<10])
+  all_clusters <- names(group_statistic)
+  more_than_10 <- setdiff(all_clusters,less_than_10)
+  
+  data.frame(sample = names(group),group=group) %>%
+    dplyr::as.tbl() %>%
+    # dplyr::filter(group %in% more_than_10) %>%
+    dplyr::rename("barcode"="sample") %>%
+    dplyr::left_join(clinical_data,by="barcode") -> group_survival_data
+  
+  color_list = color_20[1:length(more_than_10)]
+  title <- paste(data_type,"Survival for",C,"Clusters",sep=" ")
+  sur_name <-  paste(data_type,"Survival_for",C,"Clusters.png",sep="_")
+  if (length(more_than_10)>=2) {
+    group_survival_data %>%
+      dplyr::filter(group %in% more_than_10) %>%
+      fn_survival(title,color_list,sur_name,result_path,3,4)
+  }else{
+    print("Too small groups")
+  }
+  
+  print(paste(i,3,sep = "."))
+  
+  # Figure 3. mutation burden in each cancers
+  
+  print(paste(i,4,sep = "."))
+  if (length(more_than_10)>=2) {
+    comp_mat <- combn(more_than_10,2)
+    comp_list <- list()
+    m_name <-  paste(data_type,"mutation_for",C,"Clusters",sep="_")
+    for (col in 1:ncol(comp_mat)) {
+      comp_list[[col]] <- comp_mat[,col]  }
+    
+    group_survival_data %>%
+      dplyr::filter(group %in% more_than_10) %>%
+      dplyr::mutate(sm_count = ifelse(is.na(sm_count),0,sm_count)) %>%
+      # dplyr::mutate(sm_count = log2(sm_count)) %>%
+      fn_mutation_burden(group = "group",facet="~ cancer_types",value = "sm_count",color = color_list,xlab = "Mutation Burden",comp_list = comp_list,m_name = m_name,result_path = result_path,w=12)
+    
+    # Figure 4. mutation burden in all samples
+    print(paste(i,1,sep = "."))
+    m_a_name <-  paste(data_type,"all_mutation_for",C,"Clusters",sep="_")
+    group_survival_data %>%
+      dplyr::filter(group %in% more_than_10) %>%
+      dplyr::mutate(sm_count = ifelse(is.na(sm_count),0,sm_count)) %>%
+      # dplyr::mutate(sm_count = log2(sm_count)) %>%
+      fn_mutation_burden_all(group = "group",value = "sm_count",color = color_list,xlab = "Mutation Burden",comp_list = comp_list,m_a_name = m_a_name,result_path = result_path)
+  } else{
+    print("Too small groups")
+  }
+  # Figure 5. cancer distrbution in each clusters
+  print(paste(i,5,sep = "."))
+  group_survival_data %>%
+    dplyr::group_by(cancer_types,group) %>%
+    dplyr::mutate(n = n()) %>%
+    dplyr::select(group,cancer_types,n) %>%
+    unique()  %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(cancer_types) %>%
+    dplyr::mutate(n_a = sum(n)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(`Sample Composition (%)` = 100*n/n_a) -> cluster_cancers_statistic
+  
+  cluster_cancers_statistic %>%
+    ggplot(aes(x=group,y=cancer_types,fill = `Sample Composition (%)`)) +
+    geom_tile(color = "grey") +
+    geom_text(aes(label = n)) +
+    scale_fill_gradient2(
+      limit = c(0, 100),
+      breaks = seq(0, 100, 25),
+      label = c("0", "25","50","75","100"),
+      high = "red",
+      na.value = "white"
+    ) +
+    labs(x = "Cluster", y = "Cancer Types") +
+    theme(
+      panel.border = element_blank(), 
+      panel.grid.major = element_blank(), 
+      panel.grid.minor = element_blank(), 
+      axis.line = element_line(colour = "black", size = 0.5), 
+      panel.background = element_rect(fill = "white"),
+      legend.key = element_blank(),
+      legend.background = element_blank(),
+      legend.text = element_text(size = 12, colour = "black"),
+      axis.text = element_text(size = 12, colour = "black"),
+      legend.title = element_text(angle=90),
+      axis.title = element_text(size = 12,color = "black")
+    ) +
+    guides(fill = guide_colorbar(title.position = "left"))
+  ggsave(filename =paste("Sample_composition_for",C,"Clusters.png",sep="_"), path = result_path,device = "png",height = 6,width = 4)
+  print(paste(i,6,sep = "."))
+  #   return(1)
+}
