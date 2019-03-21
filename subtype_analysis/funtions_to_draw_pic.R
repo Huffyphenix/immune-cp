6e76a1038784ce244bd396f1d0e55af9e27a543e
Huffyphenix <949985347@qq.com>
2019-03-21 11:02:34 +0800

    upgrate the function for easy usage

--- subtype_analysis/funtions_to_draw_pic.R
+++ subtype_analysis/funtions_to_draw_pic.R
@@ -31,7 +31,7 @@ fn_survival <- function(data,title,color,sur_name,result_path,h,w,lx=0.8,ly=0.6)
                           panel.background = element_rect(fill = "white"),
                           legend.key = element_blank(),
                           legend.background = element_blank(),
-                          legend.text = element_text(size = 12, colour = "black"),
+                          legend.text = element_text(size = 8, colour = "black"),
                           axis.text = element_text(size = 12, colour = "black"),
                           legend.title = element_blank(),
                           axis.title = element_text(size = 12,color = "black")
@@ -46,9 +46,11 @@ fn_survival <- function(data,title,color,sur_name,result_path,h,w,lx=0.8,ly=0.6)
 
 fn_mutation_burden <- function(data,group,facet="~ cancer_types",value,color,xlab,comp_list,m_name,result_path,w=7,h=10){
   data %>%
-    ggpubr::ggboxplot(x = group, y = value,
+    ggpubr::ggboxplot(x = group, y = value,fill="white",alpha = 0,width = 0.1,
                       color = group #add = "jitter",#, palette = "npg"
     ) +
+    geom_violin(aes(fill = group),alpha = 0.5) +
+    # geom_jitter(aes(color = group),alpha = 0.2,width = 0.1) +
     # geom_point(aes(x=as.numeric(group)+b,y=sm_count,color=group),alpha = 0.5) +
     scale_x_discrete(#breaks = c(1:10),
                      labels = unique(data$group)
@@ -75,10 +77,12 @@ fn_mutation_burden <- function(data,group,facet="~ cancer_types",value,color,xla
 
 fn_mutation_burden_all <- function(data,group,value,color,xlab,comp_list,m_a_name,result_path,w=4,h=3){
   data %>%
-    ggpubr::ggboxplot(x = group, y = value,
+    ggpubr::ggboxplot(x = group, y = value,fill="white",alpha = 0,width = 0.1,
                       color = group #add = "jitter",#, palette = "npg"
     ) +
-    # geom_point(aes(x=as.numeric(group)+b,y=sm_count,color=group),alpha = 0.5) +
+    geom_violin(aes(fill = group),alpha = 0.5) +
+    geom_jitter(aes(color = group),alpha = 0.2,width = 0.1,size=0.1) +
+    geom_boxplot(fill="white",alpha = 0,width = 0.1) +
     scale_x_discrete(#breaks = c(1:10),
                      labels = unique(data$group)
                      # expand = c(0.2,0.2,0.2)
@@ -87,6 +91,9 @@ fn_mutation_burden_all <- function(data,group,value,color,xlab,comp_list,m_a_nam
     scale_color_manual(
       values = color
     )+
+    scale_fill_manual(
+      values = color
+    ) +
     # ylim(4,12) +
     ylab(xlab) +
     xlab("Group") +
@@ -94,9 +101,9 @@ fn_mutation_burden_all <- function(data,group,value,color,xlab,comp_list,m_a_nam
           # axis.title.x = element_blank(),
           strip.background = element_rect(fill = "white",colour = "white"),
           text = element_text(size = 12, colour = "black"),
-          strip.text = element_text(size = 12)) +
+          strip.text = element_text(size = 12))
     # ggpubr::stat_compare_means(label.y = 14,paired = TRUE) +
-    ggpubr::stat_compare_means(comparisons = comp_list,method = "wilcox.test",label = "p.signif")
+    # ggpubr::stat_compare_means(comparisons = comp_list,method = "wilcox.test",label = "p.signif")
   ggsave(filename =paste(m_a_name,"png",sep="."), path = result_path,device = "png",width = w,height = h)
   ggsave(filename =paste(m_a_name,"pdf",sep="."), path = result_path,device = "pdf",width = w,height = h)
 }
