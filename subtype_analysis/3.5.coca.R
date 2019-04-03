a1fe70ab4a3236091569e0b9b161ac9dfb87e688
Huffyphenix <949985347@qq.com>
2019-04-03 11:02:32 +0000

    coca for exp, cnv and methy alltag cc result

--- subtype_analysis/3.5.coca.R
+++ subtype_analysis/3.5.coca.R
@@ -7,9 +7,10 @@ library(ComplexHeatmap)
 library(magrittr)
 
 # data path ---------------------------------------------------------------
+basic_path <- "/home/huff/project/immune_checkpoint"
 
-result_path <- file.path("/project/huff/huff/immune_checkpoint/result_20171025/subtype_analysis")
-data_result_path <- "/project/huff/huff/immune_checkpoint/genelist_data"
+result_path <- file.path(basic_path,"result_20171025/subtype_analysis")
+data_result_path <- file.path(basic_path,"genelist_data")
 
 # load(file = file.path(data_result_path, ".rda_IMK_mutationburden_cancerSubtype_analysis.rda"))
 # load(file = file.path(data_result_path, ".rda_genelist_data_survival_cancer.info.rda"))
@@ -20,7 +21,7 @@ data_result_path <- "/project/huff/huff/immune_checkpoint/genelist_data"
 # load cluster results ----------------------------------------------------
 
 expr_scaled_clsuer_4 <- readr::read_tsv(file.path(result_path,"expr_scaled/Get_best_clutser_20","4_clusters_group_info.tsv"))
-methy_scaled_clsuer_6 <- readr::read_tsv(file.path(result_path,"methy_scaled/Get_best_clutser_20","6_clusters_group_info.tsv"))
+methy_scaled_clsuer_5 <- readr::read_tsv(file.path(result_path,"methy_alltags/Get_best_clutser_20","5_clusters_group_info.tsv"))
 cnv_raw_clsuer_6 <- readr::read_tsv(file.path(result_path,"CNV/Get_best_clutser_20","6_clusters_group_info.tsv"))
 
 # recode the cluster result -----------------------------------------
@@ -41,7 +42,7 @@ expr_scaled_clsuer_4.recode %>%
   dplyr::mutate(type=paste("exp",type,sep="_")) %>%
   tidyr::spread(key="type",value="value") -> expr_scaled_clsuer_4.recode.g
 
-methy_scaled_clsuer_6 %>%
+methy_scaled_clsuer_5 %>%
   dplyr::mutate(encode = purrr::map(group,.f=function(.x){
     .d <- tibble::tibble()
     if(.x == 1) {.d <- tibble::tibble(a = 1,b = 0, c = 0, d = 0, e = 0, f =0)}
@@ -49,16 +50,19 @@ methy_scaled_clsuer_6 %>%
     if(.x == 3) {.d <- tibble::tibble(a = 0,b = 0, c = 1, d = 0, e = 0, f =0)}
     if(.x == 4) {.d <- tibble::tibble(a = 0,b = 0, c = 0, d = 1, e = 0, f =0)}
     if(.x == 5) {.d <- tibble::tibble(a = 0,b = 0, c = 0, d = 0, e = 1, f =0)}
-    if(.x == 6) {.d <- tibble::tibble(a = 0,b = 0, c = 0, d = 0, e = 0, f =1)}
+    # if(.x == 6) {.d <- tibble::tibble(a = 0,b = 0, c = 0, d = 0, e = 0, f =1)}
     .d
   })) %>%
   dplyr::select(-group) %>%
-  tidyr::unnest() -> methy_scaled_clsuer_6.recode
+  tidyr::unnest() -> methy_scaled_clsuer_5.recode
 
-methy_scaled_clsuer_6.recode %>%
-  tidyr::gather(-sample,key="type",value="value") %>%
+methy_scaled_clsuer_5.recode %>%
+  unique() %>%
+  dplyr::mutate(n=1:nrow(.)) %>%
+  tidyr::gather(-barcode,-n,key="type",value="value") %>%
   dplyr::mutate(type=paste("meth",type,sep="_"))  %>%
-  tidyr::spread(key="type",value="value") -> methy_scaled_clsuer_6.recode.g
+  tidyr::spread(key="type",value="value") %>%
+  dplyr::select(-n)-> methy_scaled_clsuer_5.recode.g
 
 cnv_raw_clsuer_6 %>%
   dplyr::mutate(encode = purrr::map(group,.f=function(.x){
@@ -79,8 +83,9 @@ cnv_raw_clsuer_6.recode %>%
   dplyr::mutate(type=paste("cnv",type,sep="_"))  %>%
   tidyr::spread(key="type",value="value") -> cnv_raw_clsuer_6.recode.g
 
-expr_scaled_clsuer_4.recode.g %>%
-  dplyr::inner_join(methy_scaled_clsuer_6.recode.g,by="sample") %>%
+methy_scaled_clsuer_5.recode.g %>%
+  dplyr::rename("sample" = "barcode") %>%
+  dplyr::inner_join(expr_scaled_clsuer_4.recode.g,by="sample") %>%
   dplyr::inner_join(cnv_raw_clsuer_6.recode.g,by="sample") -> combine_data
 
 t(combine_data[,-1]) %>%  as.matrix() -> combine_data.m
@@ -99,6 +104,7 @@ colnames(combine_data.m) <- combine_data$sample
 # W <- results$distanceMatrix
 
 library(ConsensusClusterPlus)
-ConsensusClusterPlus(combine_data.m, maxK=20, reps=500,pItem=0.8,pFeature=1, title="recode.combined.ConsensusClusterplus", clusterAlg="hc",distance="pearson",seed=1262118388.71279, plot = "pdf") -> cc_res
+ConsensusClusterPlus(combine_data.m, maxK=20, reps=500,pItem=0.8,pFeature=1, title="recode.combined.ConsensusClusterplus", 
+                     clusterAlg="hc",distance="pearson",seed=1262118388.71279, plot = "pdf") -> cc_res
 
-cc_res %>% readr::write_rds(path = file.path(data_result_path, ".recode.combined.ConsensusClusterplus.rds.gz"), compress = "gz")
+cc_res %>% readr::write_rds(path = file.path(data_result_path, ".recode.combined.ConsensusClusterplus-exp-cnv-methyalltags.rds.gz"), compress = "gz")
