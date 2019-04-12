e5205957ec48952ca9565cbf7eb5c2be5cb7960f
Huffyphenix <949985347@qq.com>
2019-04-12 00:42:29 +0000

    code debugging when analyze mutation exclusive

--- autophagy_codes/03_mutation/05_mutation_exclusive.R
+++ autophagy_codes/03_mutation/05_mutation_exclusive.R
@@ -9,7 +9,7 @@ snv_path <- file.path(out_path, "m_2_snv")
 
 
 # LOAD DATA 
-ICP_highGene_snv <- readr::write_rds(file.path(snv_path,"ICP_highGene_snv.rds.gz"))
+ICP_highGene_snv <- readr::read_rds(file.path(snv_path,"ICP_highGene_snv.rds.gz"))
 
 # GET EXCLUSIVE MUTATION BETWEEN ICP AND HIGH MUTATED GENES IN EACH CANCERS ------
 library(showtext)
@@ -18,6 +18,7 @@ font_add("Arial","ARIAL.TTF") # sudo进入container，将/home/huff/fonts/中的
 library(export)
 # library(Cairo)
 fn_draw_exclusive <- function(plot_ready, V1, V2, cancer_types,p_val){
+  p_val <- signif(p_val,3)
   plot_ready %>% 
     tidyr::spread(key = symbol, value = mut) -> rank_ready
   
@@ -38,56 +39,61 @@ fn_draw_exclusive <- function(plot_ready, V1, V2, cancer_types,p_val){
     dplyr::pull(barcode) %>%
     c(plot_rank, .) -> plot_rank
   
-  showtext.begin()
+  
   plot_ready %>%
-    dplyr::mutate(mut = ifelse(mut>=2,2,mut)) %>%
+    dplyr::mutate(mut = ifelse(mut >= 2,2,mut)) %>%
     ggplot(aes(x = barcode, y = factor(1), fill = as.factor(mut))) +
-    geom_tile(color="white",width=0.9) +
+    geom_tile(color = "white",width = 0.9) +
     scale_x_discrete(limits = plot_rank) +
     facet_grid(symbol ~ .) +
     scale_fill_manual(
-      name="Mutation_type",
+      name = "Mutation_type",
       limits = c("0", "1", "2"),
-      label=c("NA", "1", ">=2"),
-      values= c( "#D6D6D6", "#FF6A6A","#FF0000")) +
-    labs(x = "", y = "", title = paste(V1, "and", V2, " SNV mutual exclusive in", cancer_types, ", p=", p_val))+
+      label = c("NA", "1", ">=2"),
+      values = c( "#D6D6D6", "#FF6A6A","#FF0000")) +
+    labs(x = "", y = "", title = paste(V1, "and", V2, "SNV mutual exclusive in", cancer_types, ", p=", p_val)) +
     theme(
-      axis.text=element_blank(),
+      axis.text = element_blank(),
       axis.title = element_blank(),
       axis.ticks = element_blank(),
       
-      strip.text.y = element_text(angle =0,hjust=0,size=11),
-      strip.text.x = element_text(size=11,angle = 90,vjust = 0),
+      strip.text.y = element_text(angle = 0,hjust = 0,size = 8),
+      strip.text.x = element_text(size = 8,angle = 90,vjust = 0),
       strip.background = element_blank(),
       
       legend.title= element_blank(),
-      legend.text = element_text(size=11),
+      legend.text = element_text(size = 8),
       legend.position = "bottom",
       
       panel.background = element_blank(),
       panel.spacing.y  = unit(0, "lines"),
       
-      text = element_text(family = "Arial")
+      text = element_text(family = "Arial",size = 8)
     )  -> p
-  showtext.end()
-  ggsave(file.path(snv_path,"mutual_exclusive",paste(fig_name,"pdf",sep=".")),device = "pdf",width=6,height=2)
-  ggsave(file.path(snv_path,"mutual_exclusive",paste(fig_name,"png",sep=".")),device = "png",width=6,height=2)
   
-  graph2ppt(x=p,file=file.path(snv_path,"mutual_exclusive",paste(fig_name,"pptx",sep=".")),width=6,height=2)
+  pdf(file.path(snv_path,"mutual_exclusive",paste(fig_name,"pdf",sep = ".")),width = 4,height = 2)
+  showtext_begin()
+  p
+  showtext_end()
+  dev.off()
+  # ggsave(file.path(snv_path,"mutual_exclusive",paste(fig_name,"pdf",sep=".")),device = "pdf",width=6,height=2)
+  ggsave(file.path(snv_path,"mutual_exclusive",paste(fig_name,"png",sep = ".")),device = "png",width = 4,height = 2)
+  
+  graph2ppt(x = p,file = file.path(snv_path,"mutual_exclusive",paste(fig_name,"pptx",sep = ".")),width = 4,height = 2)
 }
 fn_ex <- function(V1, V2, .data, cancer_types){
   # .data <- filter_cnv
-  # V1 <- 'GNAQ'
-  # V2 <- 'BTN2A1'
-  V1<-as.character(V1)
-  V2<-as.character(V2)
+  # V1 <- 'TP53'
+  # V2 <- 'HLA-E'
+  V1 <- as.character(V1)
+  V2 <- as.character(V2)
  print(V1)
   print(V2)
   .data %>% 
     dplyr::filter(symbol %in% c(V1, V2)) %>% 
     tidyr::gather(key = barcode, value = mut, -symbol) %>%
-    dplyr::mutate(mut=ifelse(is.na(mut),0,mut)) %>%
-    dplyr::mutate(mut=as.integer(mut)) -> plot_ready
+    dplyr::mutate(mut = ifelse(is.na(mut),0,mut)) %>%
+    dplyr::mutate(mut = as.integer(mut)) -> plot_ready
   plot_ready %>%
     tidyr::spread(key = symbol, value = mut) %>% 
     dplyr::select(-barcode) -> .d
@@ -101,7 +107,7 @@ fn_ex <- function(V1, V2, .data, cancer_types){
     nrow() -> nn
   .d %>%
     dplyr::filter_all(.vars_predicate = dplyr::all_vars(. != 0)) %>% 
-    nrow()-> aa
+    nrow() -> aa
   .d %>% 
     dplyr::filter_all(.vars_predicate = dplyr::any_vars(. != 0)) %>% 
     dplyr::filter_all(.vars_predicate = dplyr::any_vars(. == 0)) -> .d_an
@@ -111,7 +117,7 @@ fn_ex <- function(V1, V2, .data, cancer_types){
   c(nn = nn, an = an, na = na, aa = aa) %>% 
     cometExactTest::comet_exact_test(mutmatplot = F) -> p_val
   
-  if(p_val <= 0.05){
+  if (p_val <= 0.05) {
     fn_draw_exclusive(plot_ready, V1, V2, cancer_types,p_val)
   }
   
@@ -124,9 +130,14 @@ fn_mutal_exclusive <- function(cancer_types, ICP_SNV, highGene_SNV, cluster){
   .gene_pairs %>% 
     multidplyr::partition(cluster = cluster) %>%
     multidplyr::cluster_library("magrittr") %>%
+    multidplyr::cluster_library("showtext") %>%
+    multidplyr::cluster_library("ggplot2") %>%
+    multidplyr::cluster_library("export") %>%
     multidplyr::cluster_assign_value("fn_ex", fn_ex) %>%
     multidplyr::cluster_assign_value("combine_data", combine_data) %>%
     multidplyr::cluster_assign_value("cancer_types", cancer_types) %>%
+    multidplyr::cluster_assign_value("fn_draw_exclusive",fn_draw_exclusive) %>%
+    multidplyr::cluster_assign_value("snv_path",snv_path) %>%
     dplyr::mutate(rs = purrr::map2(Var1, Var2, .f = fn_ex, .data = combine_data, cancer_types = cancer_types)) %>% 
     dplyr::collect() %>%
     dplyr::as_tibble() %>%
